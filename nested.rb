7907c49224160b8a67861b6374ce8006bde625b9
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-18 19:30:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+	programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+	programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
