391885760464c7fb8f5777e19375e3f3098eb55f
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-18 19:48:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,6 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:languages] = ["Assembly", "COBOL", "FORTRAN"]
+	programmer_hash[:dennis_ritchie][:languages] = ["COBOL", "Assembly",  "FORTRAN"]
 	programmer_hash
 end
